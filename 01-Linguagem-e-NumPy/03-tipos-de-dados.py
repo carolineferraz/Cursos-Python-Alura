9801addabb9d3ba189e110ce100d5c37c93c98b3
Caroline Ferraz <projetos.crrz@gmail.com>
2023-01-18 19:03:49 -0300

    tipos-de-dados e conversao-de-tipos

---
+++ 01-Linguagem-e-NumPy/03-tipos-de-dados.py
@@ -0,0 +1,31 @@
+# int
+ano = 2023
+print(type(ano))
+
+# float
+km = 600.2
+print(type(km))
+
+# boolean
+carro_zero = False
+print(type(carro_zero))
+
+# string
+nome = "Jetta Variant"
+print(type(nome))
+print(nome)
+nome = "Jetta 'Variant'"
+print(nome)
+nome = 'Jetta "Variant"'
+print(nome)
+carro = '''
+    Marca
+    Modelo
+    Ano
+    Cor
+'''
+print(carro)
+
+# none
+carro = None
+print(type(carro))
---
+++ 01-Linguagem-e-NumPy/04-conversao-de-tipos.py
@@ -0,0 +1,19 @@
+# funções str() int() float()
+
+a = 10
+b = 20
+c = 'Python é '
+d = 'legal'
+
+print(a + b)
+print(c + d)
+print(type(a))
+print(type(c))
+print(c + str(a))
+print(type(str(b)))
+print(type(float(a)))
+
+print('Atenção!')
+var = 2.99
+print(var)
+print(int(var))
