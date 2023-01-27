762a40509f8baa6eb1bee95c28f75b89d2f73a7e
Caroline Ferraz <projetos.crrz@gmail.com>
2023-01-27 10:51:04 -0300

    selecoes-em-tuplas

---
+++ 02-Funcoes-Pacotes-e-Pandas/03-selecoes-em-tuplas.py
@@ -0,0 +1,11 @@
+nomes_carros = ('Jetta Variant', 'Passat', 'Crossfox', 'DSS')
+
+print(nomes_carros[0])
+print(nomes_carros[1])
+print(nomes_carros[-1])
+
+print(nomes_carros[1:3])
+
+nomes_carros = ('Jetta Variant', 'Passat', 'Crossfox', 'DSS', ('Fusca', 'Gol', 'C4'))
+print(nomes_carros[-1])
+print(nomes_carros[-1][1])
