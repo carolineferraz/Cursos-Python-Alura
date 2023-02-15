97aca45901b11e4aa0bc6d78261d70acaac2b706
Caroline Ferraz <projetos.crrz@gmail.com>
2023-02-14 22:09:57 -0300

    definindo-funcoes-que-retornam-valores

--- 02-Funcoes-Pacotes-e-Pandas/11-definindo-funcoes-que-retornam-valores.py
+++ 02-Funcoes-Pacotes-e-Pandas/11-definindo-funcoes-que-retornam-valores.py
@@ -29,11 +29,31 @@ dados = {
     'Passat': {'km': 62000, 'ano': 1999}
 }
 
-def adiciona_media_km(dataset, km_atual):
+def cria_dict_media_km(dataset, km_atual):
     resultado = {}
     for item in dataset.items():
         media_km = round(item[1]['km'] / (km_atual - item[1]['ano']), 2)
         resultado.update({ item[0]: media_km })
     return resultado
+
+print(cria_dict_media_km(dados, 2023))
+
+
+# Exercício:
+# Dado o dataset abaixo, faça uma função que adiciona ao dicionário a média de km rodados anual de cada veículo
+
+dados = {
+    'Crossfox': {'km': 35000, 'ano': 2005}, 
+    'DS5': {'km': 17000, 'ano': 2015}, 
+    'Fusca': {'km': 130000, 'ano': 1979}, 
+    'Jetta': {'km': 56000, 'ano': 2011}, 
+    'Passat': {'km': 62000, 'ano': 1999}
+}
+
+def adiciona_media_km(dataset, km_atual):
+    for item in dataset.items():
+        media_km = round(item[1]['km'] / (km_atual - item[1]['ano']), 2)
+        item[1]['km_media'] = media_km
+    return dataset
 
 print(adiciona_media_km(dados, 2023))
